bc21a7478fd2d43f73f5b608e8317e4fa6b79356
mazenmadbouly <mazen.madbouly01@gmail.com>
2025-05-14 17:53:34 +0200

    Added global pose and velocity desired topic names to topicnames.hpp

--- include/auv_core_helper/topicnames.hpp
+++ include/auv_core_helper/topicnames.hpp
@@ -13,7 +13,8 @@ const std::string obstacles = "/auv/perception/obstacles";
 // CTRL
 const std::string pose_desired_local = "/auv/local/pose_desired";
 const std::string velocity_desired_local = "/auv/local/velocity_desired";
-const std::string acceleration_desired_local = "/auv/local/acceleration_desired";
+const std::string pose_desired_global = "/auv/global/pose_desired";
+const std::string velocity_desired_global = "/auv/global/velocity_desired";
 const std::string pose_goal_local= "/auv/local/pose_goal";
 
 
