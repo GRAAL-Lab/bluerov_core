39df096d5abce8478ae80d3f1ec3700bc3606e53
Samuele Depalo <depalo.samuele@gmail.com>
2025-05-21 10:29:19 +0200

    added mission cmd service

--- include/auv_core_helper/topicnames.hpp
+++ include/auv_core_helper/topicnames.hpp
@@ -38,6 +38,7 @@ const std::string dvl_distance_actual = "/auv/dvl/distance";
 const std::string kcl_state = "/auv/kcl_state";
 
 // SERVICES
+const std::string mission_cmd_service = "/auv/service/mission_cmd";
 const std::string control_cmd_service = "/auv/service/control_cmd";
 const std::string arming_service = "/auv/service/arming";
 const std::string flight_mode_service = "/auv/service/flight_mode";
