da812c8083c26fb86c589fcefd0554a957536a60
mazenmadbouly <mazen.madbouly01@gmail.com>
2025-05-24 19:21:36 +0200

    Added definition for set_global_origin_service in topicnames.hpp

--- include/auv_core_helper/topicnames.hpp
+++ include/auv_core_helper/topicnames.hpp
@@ -30,8 +30,10 @@ const std::string kcl_state = "/auv/kcl_state";
 // SERVICES
 const std::string mission_cmd_service = "/auv/service/mission_cmd";
 const std::string control_cmd_service = "/auv/service/control_cmd";
+const std::string set_global_origin_service = "/auv/service/set_global_origin";
 const std::string arming_service = "/auv/service/arming";
 const std::string flight_mode_service = "/auv/service/flight_mode";
+
 
 
 // Actions
