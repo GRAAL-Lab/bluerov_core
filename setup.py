a93da5806b5fce3532e8b3577a3aff003a66168a
paololais <paolo.lais1@gmail.com>
2025-06-23 15:16:19 +0200

    small fix

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=[
-        'setuptools'
+        'setuptools',
         'simplekml',
     ],
     zip_safe=True,
